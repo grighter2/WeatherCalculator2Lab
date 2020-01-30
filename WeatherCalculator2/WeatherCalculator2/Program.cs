46bb751905a4fdbdc14d1815c285769ba614a5cd
grighter2 <59981783+grighter2@users.noreply.github.com>
2020-01-30 16:04:21 -0600

    Update Program.cs

--- WeatherCalculator2/WeatherCalculator2/Program.cs
+++ WeatherCalculator2/WeatherCalculator2/Program.cs
@@ -10,6 +10,7 @@ namespace WeatherCalculator2
 
             int temperature;
             int windSpeed;
+            int RelHumid;
             double windChill;
             string input = "";
 
@@ -20,18 +21,35 @@ namespace WeatherCalculator2
 
             temperature = int.Parse(input);
 
-            // Get the windspeed from the user
-            Console.WriteLine("Enter the wind speed");
+            if (temperature > 50)
+            {
+                Console.WriteLine("Enter the Relative Humidity");
+                input = Console.ReadLine();
+                RelHumid = int.Parse(input);
 
-            input = Console.ReadLine();
+                double Math1 = RelHumid * 0.094;
+                double Math2 = temperature + 61.0;
+                double Math3 = (temperature - 68.0) * 1.2;
+                double HeatIndex = (Math1 + Math2 + Math3) * 0.5;
 
-            windSpeed = int.Parse(input);
+                Console.WriteLine("It feels like " + HeatIndex);
+            }
+            else
+            {
+                // Get the windspeed from the user
+                Console.WriteLine("Enter the wind speed");
 
-            // Calculate the wind chill
+                input = Console.ReadLine();
 
-            windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+                windSpeed = int.Parse(input);
 
-            Console.WriteLine("The wind chill is " + windChill);
+
+                // Calculate the wind chill
+
+                windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+
+                Console.WriteLine("It feels like " + windChill);
+            }
         }
     }
 }
